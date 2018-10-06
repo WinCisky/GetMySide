ad1e2d0b925dbcb028a4eff237570325d463c727
sSimo <xsimone97@hotmail.it>
2018-10-06 14:55:39 +0200

    Added the color system, removed black hole, tested basic logic for the first stage (now working on shoot system)

---
+++ Assets/SCRIPTS/ShootLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootLogic : MonoBehaviour {
+
+    public GameObject laser1, laser2;
+    public Animator l1anim;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(shoot());	
+	}
+
+    public IEnumerator shoot()
+    {
+        //i shoot
+        l1anim.SetBool("shoot", true);
+        yield return new WaitUntil(() => l1anim.GetCurrentAnimatorStateInfo(0).IsName("idle"));
+        //i can shoot angain
+    }
+}
