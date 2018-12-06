698088e36cf38d5fafe23d48225890693458e74d
Simone S <xsimone97@hotmail.it>
2018-12-06 19:12:40 +0100

    new branch with light version of the game concept

---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    //main camera rotating with the view
+    public Transform targetForRotation;
+    //main cube moving in the scene
+    Rigidbody rb;
+    //main cube box collider size
+    BoxCollider bc;
+    //force multiplier to apply
+    public float force;
+    //active camera index
+    private int cam_index;
+    //array of virtual cameras
+    public GameObject[] virtual_cams;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        bc = gameObject.GetComponent<BoxCollider>();
+        bc.size = new Vector3(1, 1, 30);
+        cam_index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //direction based on rotation
+        Vector3 rightVector = targetForRotation.transform.rotation * Vector3.right;
+        Vector3 leftVector = targetForRotation.transform.rotation * Vector3.left;
+        //apply force based on rotation
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(rightVector * force);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(leftVector * force);
+        }
+        //switching the side
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SwitchSide();
+        }
+        //jumping
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.constraints == (RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY))
+        {
+            transform.position = new Vector3(transform.position.z, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.x);
+        }
+    }
+
+    //costraint change
+    //reset the cube rotation to allow smooth movement in the other dimension
+    public void SwitchSide()
+    {
+        if(rb.constraints == ( RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY))
+        {
+            rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+            transform.rotation = Quaternion.identity;
+            transform.position = new Vector3(transform.position.z, transform.position.y, transform.position.z);
+            bc.size = new Vector3(1, 1, 30);
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+            transform.rotation = Quaternion.identity;
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.x);
+            bc.size = new Vector3(30, 1, 1);
+        }
+        //change the active camera
+        SwitchCamera();
+    }
+
+    //change the active camera
+    private void SwitchCamera()
+    {
+        virtual_cams[cam_index % virtual_cams.Length].SetActive(false);
+        virtual_cams[++cam_index % virtual_cams.Length].SetActive(true);
+    }
+
+    public void Jump()
+    {
+        //jump if I'm touching the ground
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 1f);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            if(hitColliders[i].tag == "ground")
+            {
+                rb.AddForce(Vector3.up * force / 4, ForceMode.Impulse);
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/LevelCreator.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour
+{
+    //player
+    public GameObject main_cube;
+    //player transform
+    private Transform player_pos;
+    //player rigidbody
+    private Rigidbody player_rb;
+
+    //instance of cube
+    public GameObject cube_instance;
+    //current level
+    private int level;
+    //bounds of the world
+    private Vector3 world_bounds = new Vector3(10, 50, 10);
+
+    //pool of world cubes
+    private List<GameObject> pool_world_objs;
+    //size of the pool
+    private int pool_size = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_pos = main_cube.transform;
+        player_rb = main_cube.GetComponent<Rigidbody>();
+        level = 0;
+        pool_world_objs = new List<GameObject>();
+        SpawnLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //set the cubes position for the level
+    private void SpawnLevel()
+    {
+        //creation-deactivation of cubes
+        CreateResetPool();
+        //set the new pos for cubes
+        switch (level)
+        {
+            case 0:
+                GetCube().transform.position = new Vector3(0, 0, 0);
+                GetCube().transform.position = new Vector3(1, 0, 1);
+                GetCube().transform.position = new Vector3(2, 0, 2);
+                GetCube().transform.position = new Vector3(3, 0, 3);
+                GetCube().transform.position = new Vector3(4, 0, 4);
+                GetCube().transform.position = new Vector3(5, 0, 5);
+                GetCube().transform.position = new Vector3(6, 0, 6);
+                GetCube().transform.position = new Vector3(7, 0, 7);
+                GetCube().transform.position = new Vector3(8, 0, 8);
+                GetCube().transform.position = new Vector3(9, 0, 9);
+                GetCube().transform.position = new Vector3(-1, 0, -1);
+                GetCube().transform.position = new Vector3(-2, 0, -2);
+                GetCube().transform.position = new Vector3(-3, 0, -3);
+                GetCube().transform.position = new Vector3(-4, 0, -4);
+                GetCube().transform.position = new Vector3(-5, 0, -5);
+                GetCube().transform.position = new Vector3(-6, 0, -6);
+                GetCube().transform.position = new Vector3(-7, 0, -7);
+                GetCube().transform.position = new Vector3(-8, 0, -8);
+                GetCube().transform.position = new Vector3(-9, 0, -9);
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    //returns a cube (inactive or new)
+    private GameObject GetCube()
+    {
+        for (int i = 0; i < pool_world_objs.Count; i++)
+        {
+            if (!pool_world_objs[i].activeSelf)
+            {
+                pool_world_objs[i].SetActive(true);
+                return pool_world_objs[i];
+            }
+        }
+        GameObject go = Instantiate(cube_instance);
+        go.SetActive(true);
+        pool_world_objs.Add(cube_instance);
+        return go;
+    }
+
+    //add obj to pool or deactivate them
+    private void CreateResetPool()
+    {
+        foreach (var item in pool_world_objs)
+        {
+            item.SetActive(false);
+        }
+        while(pool_world_objs.Count < pool_size)
+        {
+            GameObject go = Instantiate(cube_instance);
+            go.SetActive(false);
+            pool_world_objs.Add(cube_instance);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //check if player is out of world bounds
+        if(
+            player_pos.position.z > world_bounds.z ||
+            player_pos.position.z < -world_bounds.z ||
+            player_pos.position.x > world_bounds.x ||
+            player_pos.position.x < -world_bounds.x ||
+            player_pos.position.y > world_bounds.y ||
+            player_pos.position.y < -world_bounds.y)
+        {
+            Respawn();
+        }
+    }
+
+    //reset player position to the latest spawn point
+    private void Respawn()
+    {
+        //currently i have not set spawn points
+        player_pos.position = new Vector3(0, 2, 0);
+        //remove all forces applied to the cube
+        player_rb.velocity = Vector3.zero;
+    }
+}
